e18bad245ab56b3597fbdf0dfa08fcd9d1dd6b77
David Elie-Dit-Cosaque <deliedit@redhat.com>
2023-02-07 17:33:18 -0600

    bump ptp-listerner-exports version

--- vendor/github.com/redhat-cne/ptp-listener-exports/types.go
+++ vendor/github.com/redhat-cne/ptp-listener-exports/types.go
@@ -1,7 +1,6 @@
 package exports
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -22,20 +21,3 @@ var (
 	Mu        sync.Mutex
 	AllEvents []StoredEvent
 )
-
-func (event StoredEvent) ToCsv() (out string) {
-	shortValue, ok := event[EventValues].(StoredEvent)
-	valuesString := ""
-	if ok {
-		for key, value := range shortValue {
-			valuesString += fmt.Sprintf(",%s,%s", key, value)
-		}
-	} else {
-		valuesString = fmt.Sprintf("%v", event[EventValues])
-	}
-	return fmt.Sprintf("%s,%s,%s%s",
-		event[EventTimeStamp],
-		event[EventType],
-		event[EventSource],
-		valuesString)
-}
